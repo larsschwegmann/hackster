b70dbc0c893cb272ba5dd77fc7978f539400c9f2
Lars Schwegmann <schwegmannlars@gmail.com>
2025-04-19 11:00:58 +0200

    change signin text

--- hackster-app/components/ui/SignIn.tsx
+++ hackster-app/components/ui/SignIn.tsx
@@ -9,7 +9,8 @@ export default function SignIn() {
         <div className="text-center">
             <div className="text-left mb-6">
                 <h1 className="text-4xl font-bold mb-2">Welcome to Hackster!</h1>
-                <p>Sign in with Spotify to get started</p>
+                <p>Hackster is an open-source reimplementation of the companion app of the Hitster board-game.</p>
+                <p>It allows you to make playing cards from your own Spotify playlists and use your Spotify Connect devices as speakers.</p>
             </div>
             <button className="btn btn-primary btn-block" onClick={() => {signIn("spotify")}}><LogIn/> Sign In with Spotify</button>
         </div>
